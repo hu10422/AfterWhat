eed5df538adcee1c38cca82bc6f9a1bc91143368
howitt <1662143230@qq.com>
2020-12-08 19:32:17 +0800

    Initial commit

---
+++ src/main/java/cn/hu/afterwhat/AfterwhatApplication.java
@@ -0,0 +1,13 @@
+package cn.hu.afterwhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterwhatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfterwhatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/hu/afterwhat/AfterwhatApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hu.afterwhat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterwhatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
